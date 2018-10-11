42f1ad32ee2192abc7323162c57d039e6f7a8680
aliosha7891 <aliosha7891@gmail.com>
2018-10-11 18:22:21 +0300

    Add PATCH /todo/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://aliosha7891:userab9ad1@ds125683.mlab.com:25683/todo' || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://aliosha7891:userab9ad1@ds125683.mlab.com:25683/todo' || 'mongodb://localhost:27017/TodoApp');
 
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
