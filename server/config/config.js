0358bb109d0d63fe003133b09288ca6ea7d3c84e
aliosha7891 <aliosha7891@gmail.com>
2018-10-11 19:23:51 +0300

    Setup separate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+const env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+    process.env.MONGODB_URI = 'mongodb://aliosha7891:userab9ad1@ds125683.mlab.com:25683/todo';
+}
